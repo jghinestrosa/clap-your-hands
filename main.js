c11b0987743118045aa516a1eb01ece91d6d885b
Jorge García Hinestrosa <jorgegarciahinestrosa@gmail.com>
2019-03-17 21:41:57 +0100

    Add css rules for mobile and fix collisions

--- main.js
+++ main.js
@@ -45,10 +45,10 @@
   };
 
   function checkCollision(rect1, rect2) {
-    if (rect1.x < rect2.x + rect2.width &&
-        rect1.x + rect1.width > rect2.x &&
-        rect1.y < rect2.y + rect2.height &&
-        rect1.y + rect1.height > rect2.y) {
+    if (rect1.x < rect2.x + rect2.width - rect2.width / 2 &&
+        rect1.x + rect1.width - rect1.width / 2 > rect2.x &&
+        rect1.y < rect2.y + rect2.height - rect2.height / 2 &&
+        rect1.y + rect1.height - rect1.height / 2 > rect2.y) {
       clappingHandsData.visible = true;
       topHandData.visible = false;
       backHandData.visible = false;
